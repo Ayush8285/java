828fd5f9496e2b4a47cf75e877a53f7965f73e71
Ayush8285 <113717319+Ayush8285@users.noreply.github.com>
2023-10-17 10:05:16 +0530

    new file

---
+++ PlusStar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PlusStar {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the length of the pattern");
+        int size = s.nextInt();
+
+        for (int i = 0; i < size; i++) {
+          for (int j = 0; j < size; j++) {
+            
+            if (i == size / 2) {
+              System.out.print("*");
+            }
+            
+            else {
+              if (j == size / 2) {
+                System.out.print("*");
+              } else {
+                System.out.print(" ");
+              }
+            }
+          }
+          System.out.println();
+        }
+    }
+}
